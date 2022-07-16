e0d8d7fef08b4dbf0bcf55883aecd82047e5aacb
perkasalintang52 <98840516+perkasalintang52@users.noreply.github.com>
2022-07-16 22:37:09 +0700

    Update config.js

--- config.js
+++ config.js
@@ -37,11 +37,11 @@ global.eror = '_*𝒎𝒂𝒂𝒇 𝒔𝒆𝒓𝒗𝒆𝒓 𝒆𝒓𝒓𝒐𝒓.
 
 //========Url Template Buttons=========//
 global.dtu = '𝑌𝑜𝑢𝑡𝑢𝑏𝑒'
-global.urlnya = "https://youtube.com/channel/UCW7iXlE7TgvJMIXQck4NYBQ"
+global.urlnya = "https://saweriaa.co/joceylintangg"
 
 //============= callButtons =============//
 global.dtc = '𝒄𝒂𝒍𝒍 𝒐𝒘𝒏𝒆𝒓'
-global.phn = '62 857-1313-7903'
+global.phn = '62 813-2257-7123'
 
 //============= Games ================//
 global.benar = '_*Benar*_'
